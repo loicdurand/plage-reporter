8ac14c80b9cfde8c72a55ab9c10df9aca777f9af
Loïc DURAND <loicdurand.ld@gmail.com>
2025-11-10 22:47:44 -0400

    Renommage de l'app

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:sablesargasses/main.dart';
+import 'package:kantan_gwadloup/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
